2582bc936b821ead5e4132b011f10557f89f7461
Ronaldo da Cruz <ronaldosilvacruz@gmail.com>
2022-11-19 15:05:27 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-
+import NotificationButton from "./componentes/NotificationButton"
 
 function App() {
- return(
-  <h1>olá, mundo!</h1>
- )
+  return (
+    <>
+      <h1>olá, mundo!</h1>
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
